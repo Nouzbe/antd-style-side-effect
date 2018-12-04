b45aaf32825f992afb9e3e25552f4096008ba6f0
Benjamin Amelot <bam@activeviam.com>
2018-12-04 12:18:36 +0100

    Provide a simple reproduction for the issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,7 @@
-import React from 'react';
-import {Provider} from 'react-redux';
-import {Router, Route} from 'react-router';
-import createHistory from 'history/createBrowserHistory';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Menu from './menu/Menu.jsx';
-import Page from './page/Page.jsx';
-import store from './store.js';
-import actions from './actions.js';
+import React from "react";
 
-const history = createHistory();
-history.listen((location, action) => {
-  const route = location.pathname.substr(1);
-  store.dispatch(actions.goto(route));
-});
+// import "antd/dist/antd.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <MuiThemeProvider>
-        <div
-          style={{
-            height: '100%'
-          }}
-        >
-        	<Menu/>
-        	<Page/>
-        </div>
-      </MuiThemeProvider>
-    );
-  }
-}
+const App = () => <div className="app">Please uncomment the css import</div>;
 
-class AppContainer extends React.Component {
-	render() {
-		return (
-			<Provider store={store}>
-        <Router history={history}>
-          <div 
-            style={{
-              height: '100%'
-            }}
-          >
-            <Route exact path="/" component={App}/>
-            <Route path="/:color" component={App}/>
-          </div>
-        </Router>
-			</Provider>
-		);
-	}
-}
-
-export default AppContainer;
+export default App;
